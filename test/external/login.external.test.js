c2111922abc9fa6e5851ccf34568da8f1fd3d10e
Celso TM <celso.takashi07@gmail.com>
2026-08-24 21:04:54 -0300

    aula 06 meio

--- test/external/login.external.test.js
+++ test/external/login.external.test.js
@@ -2,7 +2,7 @@ import request from 'supertest';
 import { expect } from 'chai';
 
 describe('Alunos', () => {
-  it.only('should sign up a new student', async () => {
+  it('should sign up a new student', async () => {
     const loginResponse = await request('http://localhost:3000')
       .post('/api/auth/login')
       .set('Content-Type', 'application/json')
@@ -14,11 +14,11 @@ describe('Alunos', () => {
       .set('Authorization', `Bearer ${loginResponse.body.token}`)
       .send({
         'nome': 'Ana Souza',
-        'email': 'anes.suza@example.com',
-        'matricula': '12321321',
+        'email': `ana.souza${Date.now()}@example.com`,
+        'matricula': `${Date.now()}`,
         'senha': '123456'
       })
-    
+
     expect(alunoResponse.status).to.equal(201);
     expect(alunoResponse.body).to.have.property('nome', 'Ana Souza');
     expect(alunoResponse.body).to.have.property('role', 'aluno');
